5e6b3c735758ad130b30fd8213b03cd30602baf9
Rafael Yutaka Yasuda <62554340+YasudaYutaka@users.noreply.github.com>
2021-09-17 15:14:25 -0300

    Add notes to JSON file

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
+const fs = require('fs');
 const chalk = require('chalk');
-const getNotes = require('./notes.js');
+const yargs = require('yargs');
+const notes = require('./notes.js');
 
-const command = process.argv[2];
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        notes.addNote(argv.title, argv.body)
+    }
+});
+
+// Create remove command
